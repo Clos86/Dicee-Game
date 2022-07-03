7138dfda54f98df7fc97fea74b9a076293f17d40
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-03 18:41:21 -0400

    minor edit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function gameResults() {
   document.querySelector("h1").textContent = "🚩 Player 1 Wins!"
 } else if (randomNumber1 < randomNumber2) {
   document.querySelector("h1").textContent = "Player 2 Wins 🚩!"
-} else if (randomNumber1 === randomNumber2) {
+} else {
   document.querySelector("h1").textContent = "Draw!"
 }
 };
