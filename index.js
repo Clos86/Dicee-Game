c93dbde414b58adb81401670bfb4eee2256a5d09
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-03 18:15:58 -0400

    added random number

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 document.querySelectorAll("img")[0].setAttribute("src", "images/dice6.png");
 document.querySelectorAll("img")[1].setAttribute("src", "images/dice6.png");
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
