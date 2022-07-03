fb4ab15e6590aa3b57ffd6cfe0e366cf1f9a439a
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-03 18:25:46 -0400

    changed player 2 dice randomly

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-
-document.querySelectorAll("img")[1].setAttribute("src", "images/dice6.png");
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-// console.log(randomNumber1);
 
 var randomDice1 = "images/dice" + randomNumber1 + ".png";
-console.log(randomDice1);
 
 document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
